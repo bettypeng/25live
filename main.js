939cfc5d6bf4c5babb2d13e1bf8c0ed6d392d4bb
betty_peng <betty_peng@brown.edu>
2016-01-01 23:24:04 -0500

    First commit

---
+++ main.js
@@ -0,0 +1,25 @@
+
+$(document).ready(function() { 
+
+	$(function($){
+        var addToAll = false;
+        var gallery = false;
+        var titlePosition = 'inside';
+        $(addToAll ? 'img' : 'img.fancybox').each(function(){
+            var $this = $(this);
+            var title = $this.attr('title');
+            var src = $this.attr('data-big') || $this.attr('src');
+            var a = $('<a href="#" class="fancybox"></a>').attr('href', src).attr('title', title);
+            $this.wrap(a);
+        });
+        if (gallery)
+            $('a.fancybox').attr('rel', 'fancyboxgallery');
+        $('a.fancybox').fancybox({
+            titlePosition: titlePosition
+        });
+    });(jQuery);
+
+    $('.carousel').carousel({
+    });(jQuery);
+    
+});
